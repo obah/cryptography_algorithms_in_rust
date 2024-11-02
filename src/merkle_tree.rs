a61abf98baa71e9c7bb6b6b03c2d4c93fbe476fb
obah <olusojiobah@gmail.com>
2024-11-02 07:01:59 +0100

    feat: implement a merkle tree

---
+++ src/merkle_tree.rs
@@ -0,0 +1,31 @@
+use rs_merkle::algorithms::Sha256;
+use rs_merkle::{Hasher, MerkleProof, MerkleTree};
+
+pub fn merkle() {
+    // Create tree
+    let leaf_values = ["a", "b", "c", "d"];
+
+    let leaves: Vec<[u8; 32]> = leaf_values
+        .iter()
+        .map(|x| Sha256::hash(x.as_bytes()))
+        .collect();
+
+    let merkle_tree = MerkleTree::<Sha256>::from_leaves(&leaves);
+    let merkle_root = merkle_tree
+        .root()
+        .ok_or("couldn't get the merkle root")
+        .unwrap();
+
+    println!("The root of the merkle tree is: {:?}", merkle_root);
+
+    // Generate and verify proof
+    let index_to_prove = [1];
+    let leaf_to_prove = *leaves.get(1).ok_or("Cant get leaf at index").unwrap();
+    let merkle_proof = merkle_tree.proof(&index_to_prove);
+
+    let proof_bytes = merkle_proof.to_bytes();
+
+    let proof = MerkleProof::<Sha256>::try_from(proof_bytes).unwrap();
+
+    assert!(proof.verify(merkle_root, &index_to_prove, &[leaf_to_prove], leaves.len()));
+}
